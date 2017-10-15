bcde689da29de36387e3ae416deb66928a2d0785
Kevin <me@kevinho.net>
2017-10-15 15:55:55 -0700

    content page template changes

--- wp-content/themes/pfl/page_content.php
+++ wp-content/themes/pfl/page_content.php
@@ -49,21 +49,23 @@ get_header(); ?>
 
 			<div class="row">
 				<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 hero">
-					<em>Brilliant Behaviours</em>
+					<em><?php the_title(); ?></em>
 				</div>
 			</div>
 
 			<div class="row content-row">
 
 				<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 subtitle">
-					<em>Finding Bright and Gifted Students with Brilliant Behaviours</em>
+					<em><?php the_field('subtitle'); ?></em>
 				</div>
 
 				<div class="container">
 					<div class="col-xs-12 col-sm-10 col-sm-offset-1 content-body">
-						<p>Historically, testing has been the most common lens used to inspect students’ potential. Today, concerns regarding the use of many tests with students from culturally and economically diverse backgrounds has reduced confidence and reliance on them.[112] More contemporary approaches that respect this diversity can be supported by using the Tools provided here.</p>
-						<p>Students indicate their need for more challenge in a variety of ways, some more direct than others. Some students demand, some ask and some have to be found.  This section provides guidance for stimulating students’ potentials during classroom activities.  It includes alternate formats of a tool, the Brilliant Behaviors checklist, for observing students to assess the nature and extent of those behaviors during that activity.</p>
-						<p>Students should be observed for signs of the Brilliant Behaviors while they are engaged in their strongest subject(s), their passions. Students’ greatest academic strengths are the areas in which there is the greatest need for curriculum differentiation.</p>
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<?php echo the_content();?>
+
+					<?php endwhile; endif; ?>
 					</div>
 				</div>
 			</div>
