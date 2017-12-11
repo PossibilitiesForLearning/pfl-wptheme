dfeb8f3856a2c1b74379241774482471e565ef77
Kevin <me@kevinho.net>
2017-12-10 19:03:29 -0800

    search only returning 1 result wtf

--- wp-content/themes/pfl/search.php
+++ wp-content/themes/pfl/search.php
@@ -1,3 +1,5 @@
+<?php /* Template Name: Search Results */ ?>
+
 <?php
 /**
  * Template for displaying all pages
@@ -51,24 +53,25 @@ get_header(); ?>
 					<?php
                     $s=get_search_query();
                     $args = array(
-                                    's' =>$s
-                                );
+                        's' =>$s
+                    );
                         // The Query
                     $the_query = new WP_Query( $args );
                     if ( $the_query->have_posts() ) {
-                            _e("<h2 style='color: #2f343b'>Matches for: '".get_query_var('s')."'</h2>");
-                            while ( $the_query->have_posts() ) {
-                               $the_query->the_post();
-                                     ?>
-                                        <li>
-                                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                        </li>
-                                     <?php
+                            _e("<h2 style='color: #2f343b'>Matches for: '" . get_query_var('s') . "'</h2>");
+                            while ( $the_query->have_posts() : $the_query->the_post()) {                               
+                                ?>
+                                <li>
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </li>
+                                <?php
                             }
                         }else{
                     ?>
                             <h4 style="color: #2f343b">No results found. <a href="/">Click here</a> to return to home page.</h4>
-                    <?php } ?>
+                    <?php } 
+                    wp_reset_query();
+                    ?>
 					</div>
 				</div>
 			</div>
